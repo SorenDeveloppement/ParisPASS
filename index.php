ad8dfa3aa00a2acd3c8d03be24a905a38b022a21
SorenDeveloppement <sorenel@yahoo.com>
2023-04-15 16:27:13 +0200

    Adding sources files

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php session_start() ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Paris CT Pass</title>
+
+    <script>
+        alert("Il s'agit d'un site fictif réalisé pour un projet d'anglais !")
+    </script>
+</head>
+<body>
+
+    <div class="top-bar">
+        <img src="img/logo.png" class="left" style="width: 200px; height: 70px; margin-left: 15px; margin-top: 5px;">
+        <h1 class="Center">Paris PASS</h1>
+        <span></span>
+    </div>
+
+    <div class="menubtn">
+        <div class="menu">
+            <a href="login.php">Se connecter</a>
+            <a href="#">Se déconnecter</a>
+        </div>
+    </div>
+
+    <br><br><br><br><br><br>
+
+    <article class="content">
+        <article class="cards">
+            <h1>Vos cartes :</h1>
+            <p>Vous n'avez pas encore de cartes</p>
+        </article>
+    </article>
+    
+</body>
+</html>
