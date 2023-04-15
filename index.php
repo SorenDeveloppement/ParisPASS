0414852424792bba706b6b5e64d05362d0adc378
SorenDeveloppement <sorenel@yahoo.com>
2023-04-15 22:36:19 +0200

    Saving files

--- index.php
+++ index.php
@@ -23,6 +23,9 @@
 
     <div class="menubtn">
         <div class="menu">
+            <a href="#cards">Mes cartes</a>
+            <a href="#monuments">Monuments</a>
+            <a href="#museum">Musées</a>
             <a href="login.php">Se connecter</a>
             <a href="#">Se déconnecter</a>
         </div>
@@ -31,9 +34,67 @@
     <br><br><br><br><br><br>
 
     <article class="content">
-        <article class="cards">
-            <h1>Vos cartes :</h1>
-            <p>Vous n'avez pas encore de cartes</p>
+        <article class="cards" id="cards">
+            <h1>Mes cartes :</h1>
+            <p>Vous ne possédez pas encore de carte Paris PASS</p>
+        </article>
+
+        <br><br>
+
+        <span id="monuments"></span>
+        <br><br><br><br>
+
+        <article class="articles">
+            <h1 class="h1">Monuments à visiter :</h1>
+            <article class="article">
+                <img src="monument.png" style="width: 350px; height: 350px;" class="img">
+                <div class="info">
+                    <h1>Monument</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem iusto, error illum enim cupiditate modi eveniet. Vitae officiis, dolor, reiciendis labore illum perspiciatis voluptatum eos eaque enim maiores, velit autem?</p>
+                </div>
+            </article>
+            <article class="article">
+                <img src="monument.png" style="width: 350px; height: 350px;" class="img">
+                <div class="info">
+                    <h1>Monument</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem iusto, error illum enim cupiditate modi eveniet. Vitae officiis, dolor, reiciendis labore illum perspiciatis voluptatum eos eaque enim maiores, velit autem?</p>
+                </div>
+            </article>
+            <article class="article">
+                <img src="monument.png" style="width: 350px; height: 350px;" class="img">
+                <div class="info">
+                    <h1>Monument</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem iusto, error illum enim cupiditate modi eveniet. Vitae officiis, dolor, reiciendis labore illum perspiciatis voluptatum eos eaque enim maiores, velit autem?</p>
+                </div>
+            </article>
+        </article>
+
+        <span id="museum"></span>
+        <br><br><br><br>
+
+        <article class="articles">
+            <h1 class="h1">Musées à visiter :</h1>
+            <article class="article">
+                <img src="monument.png" style="width: 350px; height: 350px;" class="img">
+                <div class="info">
+                    <h1>Musée</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem iusto, error illum enim cupiditate modi eveniet. Vitae officiis, dolor, reiciendis labore illum perspiciatis voluptatum eos eaque enim maiores, velit autem?</p>
+                </div>
+            </article>
+            <article class="article">
+                <img src="monument.png" style="width: 350px; height: 350px;" class="img">
+                <div class="info">
+                    <h1>Musée</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem iusto, error illum enim cupiditate modi eveniet. Vitae officiis, dolor, reiciendis labore illum perspiciatis voluptatum eos eaque enim maiores, velit autem?</p>
+                </div>
+            </article>
+            <article class="article">
+                <img src="monument.png" style="width: 350px; height: 350px;" class="img">
+                <div class="info">
+                    <h1>Musée</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem iusto, error illum enim cupiditate modi eveniet. Vitae officiis, dolor, reiciendis labore illum perspiciatis voluptatum eos eaque enim maiores, velit autem?</p>
+                </div>
+            </article>
         </article>
     </article>
     
