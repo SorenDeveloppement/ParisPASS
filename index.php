d393afde60ee7a3996acf60acddb63bac16693ae
SorenDeveloppement <sorenel@yahoo.com>
2023-04-16 10:05:42 +0200

    saving files

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/card.css">
     <title>Paris CT Pass</title>
 
     <script>
@@ -26,20 +27,35 @@
             <a href="#cards">Mes cartes</a>
             <a href="#monuments">Monuments</a>
             <a href="#museum">Musées</a>
-            <a href="login.php">Se connecter</a>
-            <a href="#">Se déconnecter</a>
+            <a href="login.php" class="bottom">Se connecter</a>
         </div>
     </div>
 
     <br><br><br><br><br><br>
 
     <article class="content">
-        <article class="cards" id="cards">
+
+        <span id="cards"></span>
+        <br><br><br><br>
+
+        <article class="cards-container">
             <h1>Mes cartes :</h1>
             <p>Vous ne possédez pas encore de carte Paris PASS</p>
-        </article>
 
-        <br><br>
+            <div class="cards">
+                <div class="card">
+                    <div class="front-card card-box">
+                        <img src="img/paris-pass.png" class="card-img">
+                    </div>
+                    <div class="back-card card-box">
+                        <p class="card-text">Nom : DeTest</p>
+                        <p class="card-text">Prénom : Test</p>
+                        <p class="card-text">Carte crée le : 01/01/2023</p>
+                        <p class="card-text">UUID : 6uhi7 y7hb4 tyei5</p>
+                    </div>
+                </div>
+            </div>
+        </article>
 
         <span id="monuments"></span>
         <br><br><br><br>
@@ -47,21 +63,21 @@
         <article class="articles">
             <h1 class="h1">Monuments à visiter :</h1>
             <article class="article">
-                <img src="monument.png" style="width: 350px; height: 350px;" class="img">
+                <img src="img/monument.png" style="width: 350px; height: 350px;" class="img">
                 <div class="info">
                     <h1>Monument</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem iusto, error illum enim cupiditate modi eveniet. Vitae officiis, dolor, reiciendis labore illum perspiciatis voluptatum eos eaque enim maiores, velit autem?</p>
                 </div>
             </article>
             <article class="article">
-                <img src="monument.png" style="width: 350px; height: 350px;" class="img">
+                <img src="img/monument.png" style="width: 350px; height: 350px;" class="img">
                 <div class="info">
                     <h1>Monument</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem iusto, error illum enim cupiditate modi eveniet. Vitae officiis, dolor, reiciendis labore illum perspiciatis voluptatum eos eaque enim maiores, velit autem?</p>
                 </div>
             </article>
             <article class="article">
-                <img src="monument.png" style="width: 350px; height: 350px;" class="img">
+                <img src="img/monument.png" style="width: 350px; height: 350px;" class="img">
                 <div class="info">
                     <h1>Monument</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem iusto, error illum enim cupiditate modi eveniet. Vitae officiis, dolor, reiciendis labore illum perspiciatis voluptatum eos eaque enim maiores, velit autem?</p>
@@ -75,21 +91,21 @@
         <article class="articles">
             <h1 class="h1">Musées à visiter :</h1>
             <article class="article">
-                <img src="monument.png" style="width: 350px; height: 350px;" class="img">
+                <img src="img/monument.png" style="width: 350px; height: 350px;" class="img">
                 <div class="info">
                     <h1>Musée</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem iusto, error illum enim cupiditate modi eveniet. Vitae officiis, dolor, reiciendis labore illum perspiciatis voluptatum eos eaque enim maiores, velit autem?</p>
                 </div>
             </article>
             <article class="article">
-                <img src="monument.png" style="width: 350px; height: 350px;" class="img">
+                <img src="img/monument.png" style="width: 350px; height: 350px;" class="img">
                 <div class="info">
                     <h1>Musée</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem iusto, error illum enim cupiditate modi eveniet. Vitae officiis, dolor, reiciendis labore illum perspiciatis voluptatum eos eaque enim maiores, velit autem?</p>
                 </div>
             </article>
             <article class="article">
-                <img src="monument.png" style="width: 350px; height: 350px;" class="img">
+                <img src="img/monument.png" style="width: 350px; height: 350px;" class="img">
                 <div class="info">
                     <h1>Musée</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem iusto, error illum enim cupiditate modi eveniet. Vitae officiis, dolor, reiciendis labore illum perspiciatis voluptatum eos eaque enim maiores, velit autem?</p>
