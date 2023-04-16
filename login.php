0c64ec44b3fe191a741e4c36e324d4f6c35769e3
SorenDeveloppement <sorenel@yahoo.com>
2023-04-16 16:34:58 +0200

    saving files

---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+    <title>Login</title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="card">
+            <div class="username">
+                <h2>Email</h2>
+                <input type="email" name="email" id="email" placeholder="Email" class="field">
+            </div>
+
+            <br>
+
+            <div class="password">
+                <h2>Password</h2>
+                <input type="password" name="pass" id="pass" placeholder="Username" class="field">
+            </div>
+
+            <br>
+            <input type="submit" class="submit">
+        </div>
+    </div>
+    
+</body>
+</html>
